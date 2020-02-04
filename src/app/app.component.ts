5a9121fb8cf87760960e10c1fd17a01d4bc60ebe
root <jhomartinez@umariana.edu.co>
2020-02-04 16:48:40 -0500

    cliente angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-app';
+  title = 'Welcome Jhon';
+  curso = 'Curso de spring 5 con angular 8';
+  profesor = 'Andres';
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from "./cliente"
+export const CLIENTES: Cliente[] =[
+    {id: 1,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 2,nombre: 'Mac', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 3,nombre: 'Billy', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 4,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 5,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 6,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 7,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 8,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 9,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'},
+    {id: 10,nombre: 'Jhon', apellido: 'Martinez',email:'jhon@gmail.com', createAt: '2017-12-11'}
+  ];
